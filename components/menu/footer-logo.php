2d0683461c635734a3ef0bfe4f87095a5486eed3
Leandro OC <oliva1337@gmail.com>
2023-08-08 11:05:49 -0300

    advc-95 -> Adiciona suporte a footer logo especifico para mobile views

--- components/menu/footer-logo.php
+++ components/menu/footer-logo.php
@@ -1,8 +1,15 @@
 <?php if(!empty($logo = get_theme_mod('footer_logo', 0))): ?>
   <div class="pa-brand">
-    <a href="/" class="d-flex flex-column justify-content-center">
+    <a href="/" class="d-lg-flex d-none flex-column justify-content-center">
       <?= wp_get_attachment_image($logo, 'full') ?>  
     </a>
+
+        <?php if (!empty($footerLogoMobile = get_theme_mod('footer_logo_mobile', 0))): ?>
+      <a href="/" class="d-flex d-lg-none flex-column justify-content-center">
+          <?= wp_get_attachment_image($footerLogoMobile, 'full') ?>
+      </a>
+
+      <?php endif; ?>
   </div>
 
   <hr class="mt-4 mb-4">
